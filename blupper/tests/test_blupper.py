97f9406e1deaf5f5946323d44bb6f16bf53e6481
kirill.krivushin <kirill.krivushin@L-867150.local>
2020-03-22 15:30:07 -0600

    Add a A_inverse test

--- blupper/tests/test_blupper.py
+++ blupper/tests/test_blupper.py
@@ -9,10 +9,10 @@
 #         self.assertTrue(isinstance(s, basestring))
 
 import pandas as pd
-import pandas as np
+import numpy as np
 import nose
-from blupper.mock_data import ex_eight_animals_data_table
-from blupper.mme import mme_solution
+from blupper.mock_data import ex_eight_animals_data_table, ex_pedigree_six_animals
+from blupper.mme import mme_solution, A_inverse_no_inbreeding
 
 
 def test_mme_solution():
@@ -28,3 +28,20 @@ def test_mme_solution():
     blups_true = np.array([0.098, -0.019, -0.041, -0.009, -
                            0.186,  0.177, -0.249,  0.183])
     assert (blup_predicted == blups_true).all()
+
+
+def test_A_inverse_no_inbreeding():
+    """Check inversion of A not accounting for inbreeding
+    """
+
+    df = ex_pedigree_six_animals()
+    result = A_inverse_no_inbreeding(df)
+
+    ground_truth = np.array([[1.83, 0.5, -1, -0.67,  0, 0],
+                             [0.5, 2, -1,  0,  0.5, -1],
+                             [-1, -1, 2.5,  0.5, -1, 0],
+                             [-0.67, 0, 0.5, 1.83, -1, 0],
+                             [0, 0.5, -1, -1, 2.5, -1],
+                             [0, -1, 0, 0, -1, 2]])
+
+    assert np.array_equal(result.round(2), ground_truth)
