f9f6260638bfb31566f25a16fb8c1ca7aa1685d2
kirill.krivushin <kirill.krivushin@L-867150.local>
2020-03-22 22:36:09 -0600

    Fix typos

--- blup_generator.py
+++ blup_generator.py
@@ -5,14 +5,22 @@ from pandas import read_csv
 if __name__ == '__main__':
 
     @click.command()
-    @click.option('--input_csv', default='./blupper/tests/test_data/eight_animals_data.csv',
-                  help="Table acontaining pedegree and traits information")
-    @click.option('--response_var', default='WWG', help='Response variable')
-    @click.option('--fixed_factor', default=['Sex'], help='Fixed factor in the mixed model')
+    @click.option('--input_csv',
+                  default='./blupper/tests/test_data/eight_animals_data.csv',
+                  help="Table containing pedegree and trait information")
+    @click.option('--response_var', default='WWG',
+                  help='Response variable')
+    @click.option('--fixed_factor', default=['Sex'],
+                  help='Fixed factor in the mixed model')
     @click.option('--sigma_sq_a', default=20, help='Variance(a)')
     @click.option('--sigma_sq_e', default=40, help='Variance(e)')
     @click.option('--output_csv', help='Name of output CSV file')
-    def run_mme(input_csv, sigma_sq_a, sigma_sq_e, response_var, fixed_factor, output_csv):
+    def run_mme(input_csv, sigma_sq_a, sigma_sq_e, response_var, fixed_factor,
+                output_csv):
+        """Mixed linear model for the prediction of breeding values and 
+        estimation of fixed effects under an animal model. The model is 
+        implemented and tested on data from Chapter 3 (Mrode RA, 2014).
+        """
         df = read_csv(input_csv)
         tmp = df.Dam[df.Dam != 'Unknown'].map(int)
         df.Dam = [int(i) if i != 'Unknown' else i for i in df.Dam]
